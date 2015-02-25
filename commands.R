a71d3bcd0a0ed8e39166fc962312e019accbd1e4
Jonas Hansen <jonas.hansen@bluewin.ch>
2015-02-25 22:27:35 +0100

    rename file

--- commands.R
+++
@@ -1,7 +0,0 @@
-# Run local
-library(shiny)
-runApp("~/Development/workspaces/R/shiny/results/")
-
-# Deploy
-library(shinyapps)
-shinyapps::deployApp("~/Development/workspaces/R/shiny/results/")
---
+++ deploy.R
@@ -0,0 +1,2 @@
+library(shinyapps)
+shinyapps::deployApp("~/Development/workspaces/R/shiny/results/")
